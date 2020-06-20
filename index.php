28b599ef5886190033d392eefdbb0ab81cdbf238
Mardoniosc <mardonio@live.com>
2020-06-20 13:42:55 -0300

    :v: Adicionado um menu para teste dos arquivos

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+    <title>Menu Dropdown Horizontal - Linha de Código</title>
+     <!-- Aqui chamamos o nosso arquivo css externo -->
+    <link rel="stylesheet" type="text/css"  href="estilo.css" />
+    	
+
+    <!--[if lte IE 8]>
+ <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+ <![endif]-->   
+</head>
+<body>
+  <nav>
+    <ul class="menu">
+          <li><a href="/ChamadaCurl">Chamada Curl</a></li>
+          <li><a href="/ExemploMvc">Exemplo MVC</a></li>
+          <li><a href="/GeradorDeSenha">Gerador de senha</a></li>
+          <li><a href="#">Não faz nada</a></li>                
+  </ul>
+  </nav>
+</body>
+</html>
